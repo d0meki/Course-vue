a20331a33fc92aa7e488100f19163d70c3a6b8e5
Freddy Arriaga <domeki151190@gmail.com>
2024-06-02 18:42:57 -0400

    landing page completed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
-<template>
-  <!-- <h1 className="text-3xl font-bold underline">Hello world!</h1> -->
+  <!-- <template>
+<h1 className="text-3xl font-bold underline">Hello world!</h1> -->
   <!-- <IndecisionView /> -->
   <!-- <CounterComponent :value="5" /> -->
   <!-- <CounterScript :value="5" /> -->
-  <PokemonGame/>
+  <!-- <PokemonGame/> -->
+ <!--  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/features">Features</router-link></li>
+      <li><router-link to="/pricing">Pricing</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
+    </ul>
+  </nav>
+  <main class="bg-state-100" >
+    <RouterView />
+  </main> 
+</template>-->
+
+<template>
+  <RouterView />
 </template>
 
 <script lang="ts" setup>
 // import CounterComponent from '../src/components/CounterComponent.vue'
 // import CounterScript from './components/counter/CounterScript.vue'
 // import IndecisionView from './views/IndecisionView.vue'
-import PokemonGame from './modules/pokemons/pages/PokemonGame.vue'
+// import PokemonGame from './modules/pokemons/pages/PokemonGame.vue'
 </script>
---
+++ src/router/index.ts
@@ -0,0 +1,75 @@
+import isAuthenticatedGuard from '@/modules/auth/guards/is-authenticated.guard';
+import NotFound404 from '@/modules/common/pages/NotFound404.vue';
+import HomePage from '@/modules/landyng/pages/HomePage.vue';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
+
+const route = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('@/modules/landyng/layouts/LandingLayout.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomePage,
+        },
+        {
+          path: '/features',
+          name: 'features',
+          component: () => import('@/modules/landyng/pages/FeaturesPage.vue'),
+        },
+        {
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import('@/modules/landyng/pages/PricingPage.vue'),
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: () => import('@/modules/landyng/pages/ContacPage.vue'),
+        },
+        {
+          path: '/pokemon/:id',
+          name: 'pokemon',
+          beforeEnter: [isAuthenticatedGuard],
+          // props: true,
+          props: (route) => {
+            // console.log({ route });
+            const id = +route.params.id;
+
+            return isNaN(id) ? { id: 1 } : { id };
+          },
+          component: () => import('@/modules/pokemons/pages/PokemonPage.vue'),
+        },
+      ],
+    },
+    {
+      path: '/auth',
+      redirect: { name: 'login' }, //'/login',
+      component: () => import('@/modules/auth/layouts/AuthLayout.vue'),
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('@/modules/auth/pages/LoginPage.vue'),
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('@/modules/auth/pages/RegisterPage.vue'),
+        },
+      ],
+    },
+    // Not Found
+    {
+      path: '/:pathMatch(.*)*',
+      // redirect: '/',
+      component: NotFound404,
+    },
+  ],
+});
+
+export default route;
